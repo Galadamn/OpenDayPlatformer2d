c0b0df0a66b7d292ed6b031304d0d4c1d2e032c1
Galadan <arturas026@gmail.com>
2025-10-15 11:43:40 +0200

    initial commit

---
+++ Assets/Scripts/Player/InputManager.cs
@@ -0,0 +1,34 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    private InputSystem_Actions _inputSystem;
+
+    public float Horizontal;
+    public bool Jump,JumpHeld;
+    public bool DashSprint;
+    public bool Attack;
+    public bool Interact;
+
+    private void Update()
+    {
+        Horizontal = 
+            _inputSystem.Player.Move.
+                ReadValue<Vector2>().x;
+        
+        Jump = _inputSystem.Player.Jump.
+            WasPressedThisFrame();
+        JumpHeld = _inputSystem.Player.Jump.
+            IsPressed();
+        DashSprint = _inputSystem.Player.Sprint.
+            WasPressedThisFrame();
+
+
+    }
+
+    private void Awake() { _inputSystem = new  InputSystem_Actions(); } 
+    private void OnEnable() { _inputSystem.Enable(); }
+    private void OnDisable() { _inputSystem.Disable(); } 
+}
